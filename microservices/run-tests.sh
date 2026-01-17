dee25b63e00b008cec0533689bd9c1477bbe49f7
Antonio Garcia-Dominguez <a.garcia-dominguez@york.ac.uk>
2026-01-17 15:17:28 +0000

    Add Compose and run-tests.sh script

---
+++ microservices/run-tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+for i in dummyRates iot reactive; do
+  pushd "$i"
+  ./gradlew build dockerBuild
+  popd
+done
+
+docker compose down -v
+docker compose up --wait
+pushd end2end-tests
+./gradlew test
+docker compose down -v
+popd
