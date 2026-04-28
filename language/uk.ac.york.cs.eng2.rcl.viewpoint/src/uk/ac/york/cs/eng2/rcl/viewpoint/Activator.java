3d04a3b7c61bb5d41f763b013b2b1614575dec16
Antonio Garcia-Dominguez <a.garcia-dominguez@york.ac.uk>
2026-04-28 15:12:35 +0100

    Replace reactive.viewpoint with rcl.viewpoint

--- language/uk.ac.york.cs.eng2.rcl.viewpoint/src/uk/ac/york/cs/eng2/rcl/viewpoint/Activator.java
+++ language/uk.ac.york.cs.eng2.rcl.viewpoint/src/uk/ac/york/cs/eng2/rcl/viewpoint/Activator.java
@@ -13,7 +13,7 @@ import org.osgi.framework.BundleContext;
  */
 public class Activator extends AbstractUIPlugin {
     // The plug-in ID
-    public static final String PLUGIN_ID = "uk.ac.york.cs.eng2.reactive.viewpoint";
+    public static final String PLUGIN_ID = "uk.ac.york.cs.eng2.rcl.viewpoint";
 
     // The shared instance
     private static Activator plugin;
